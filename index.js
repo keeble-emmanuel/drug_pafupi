a47081e9ca07d82aefe0606bf03f8f7880c21497
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-05-09 20:29:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ app.use(express.static('frontend'))
 
 //search input controller
 app.post('/', contoller.searchDrug)
+
 //first page display
 app.get('/',)
+
 //
 app.post('/new-product', (req, res)=>{
     const newDrugEntry = req.body
     console.log(newDrugEntry)
 })
 
-app.post('/sign-in', (req, res)=>{
-    const newDrugEntry = req.body
-    console.log(newDrugEntry)
-})
+app.post('/sign-in', contoller.signInfunx)
 
 app.listen(PORT, ()=>{
     console.log('app listening')
