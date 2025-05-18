454a71cbd3965d4ae87ca3e97c507021afc15014
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-05-18 08:53:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.get('/new_product', (req, res)=>{
 
 //
 app.post('/new-product', contoller.createNewDrug)
-
+//searched page
+app.get('/searched-page', contoller.searchedPage)
+//
 app.post('/sign-in', contoller.signInfunx)
 app.get('/getproducts/:user_id',  contoller.getUserproducts )
 
