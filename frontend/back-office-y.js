acfc7d63f3e2660c7f9201efedda4ba0d5e6e80d
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-05-27 20:37:44 +0200

    Update back-office-y.js

--- frontend/back-office-y.js
+++ frontend/back-office-y.js
@@ -1,92 +1,65 @@
-const genericName = document.getElementById('generic-name');
-const tradeName = document.getElementById('trade-name');
-const drugStrength = document.getElementById('strength');
-const enterNewEntry = document.getElementById('entry-btn')
-const productsDisplay =  document.getElementById('products-display');
-const stockStat=  document.getElementById('stock-stat');
-const pcategory = document.getElementById('category');
-const route = document.getElementById('route');
-const dialogMsg = document.getElementById("dialog-msg")
+const regAccounts = document.getElementById("registered-accounts");
+const nameOfUser = document.getElementById('name');
+const locationOfUser = document.getElementById('location')
+const contactOfUser = document.getElementById('contact')
+const cityOfUser = document.getElementById('city')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm');
+const createAccBtn = document.getElementById("create-account")
 
-const confirmDeleteDialog = document.getElementById('confirm-delete-dialog');
-const confirmDeleteButton =  document.getElementById('confirm-delete-btn')
-console.log(route.value)
 
-const personData = JSON.parse(localStorage.getItem("person-info")) || [];
-const productToDelete = JSON.parse(localStorage.getItem("product-delete")) || [];
-const postNewEntry =async()=>{
-    const post = await fetch(`${window.location.origin}/new-product`,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            genericName: genericName.value,
-            tradeName: tradeName.value,
-            drugStrength: drugStrength.value,
-            drugCategory: pcategory.value,
-            drugStockstatus: stockStat.value, 
-            route:route.value,
-            
-            user_id: personData[0].user_id
-        })
-    })
-    const response =  await post.json()
+
+const fetchRegisterOfAccounts =async()=>{
+    const fetchs = await fetch(`${window.location.origin}/all-users`)
+    const response = await fetchs.json()
     console.log(response)
-}
-
-
-const getUserProducts = async()=>{
-    const user_id= personData[0].user_id
-    const getproducts = await fetch(`${window.location.origin}/getproducts/${user_id}`)
-    const data = await getproducts.json()
-    console.log(data)
-    data.forEach((el)=>{
-        productsDisplay.innerHTML +=`
-            <li>
-                <p>${el.genericName} ${el.drugStrength} <b>(${el.tradeName})</b> ${el.route} </p> 
-                <button id='${el._id}' class="delete-edit" onclick="deleteDialog('${el.tradeName}')">  &#9932;  </button>
-            </li>
+    response.forEach((el)=>{
+        regAccounts.innerHTML += `
+            <div class="individual-account">
+                    <img src=""/>
+                    <p class="text-center">${el.name} (${el.location})</p>
+                    <div id="button-div">
+                        <button class="text-center view-user">delete</button> 
+                        <button class="text-center view-user">visit account</p>  
+                    </div>
+                    
+                    
+                </div>
         `
     })
 }
-getUserProducts()
-//delete product function
+const postNewaccount =async()=>{
+    try{
+        const postss = await fetch(`${window.location.origin}/new-user`, 
+        {
+             method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: nameOfUser.value,
+                city: cityOfUser.value,
+                locationOfUser: locationOfUser.value,
+                phone: contactOfUser.value,
+                username: username.value,
+                password:password.value,
 
-const deleteDialog=(par)=>{
-    const idto = event.target.id
-    console.log(idto)
-    productToDelete.unshift(idto)
-    localStorage.setItem('product-delete', JSON.stringify(productToDelete))
-    dialogMsg.textContent = `delete ${par}`
-    confirmDeleteDialog.showModal()
-}
-//deleteDialog();
-const deleteProduct = async(par)=>{
-    const del = await fetch(`${window.location.origin}/deleteProduct/${par}`)
-    const response = await del.json()
-    console.log(response)
-    
-}
-
-
-
-enterNewEntry.addEventListener('click', ()=>{
-    if( genericName.value && tradeName.value && drugStrength){
-        postNewEntry()
-        //console.log(pcategory.value)
-    }else{
-        alert('write full details')
+            })
+        }
+        
+    )
+        //const response = postss.json();
+        //console.log(response)
+    }
+    catch(err){
+        console.error(err)
     }
     
-    window.location.reload()
     
-    
+}
+
+fetchRegisterOfAccounts()
+createAccBtn.addEventListener('click', ()=>{
+    postNewaccount()
 })
-
-confirmDeleteButton.addEventListener("click", ()=>{
-    window.location.reload()
-    deleteProduct(productToDelete[0])
-})
-
-
