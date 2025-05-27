a43f07576831270bcc38fefe50597982910b378b
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-05-27 07:41:24 +0200

    Update master_account.js

--- frontend/master_account.js
+++ frontend/master_account.js
@@ -1,18 +1,27 @@
-const registeredAccounts = document.getElementById("registered_accounts");
+const registeredAccounts = document.getElementById("registered_accountsx");
+const name = document.getElementById('name');
+const location = document.getElementById('location')
+const contact = document.getElementById('contact')
+const city = document.getElementById('city')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm')
 
 
 
 const fetchRegisterOfAccounts =async()=>{
-    const fetch = await fetch('/all-accounts')
-    const response = await fetch.json()
+    const fetchs = await fetch(`${window.location.origin}/all-users`)
+    const response = await fetchs.json()
+    console.log(response)
     response.forEach((el)=>{
-        registeredAccounts.innerHTML = `
-            <div>
-
-                <img src=""/>
-                <p>details</p>
-
-            </div>
+        registeredAccounts.innerHTML += `
+            <div class="individual-account">
+                    <img src=""/>
+                    <p class="text-center">${el.name} (${el.location})</p>
+                    <button class="text-center view-user">visit account</p>
+                </div>
         `
     })
 }
+
+fetchRegisterOfAccounts()
