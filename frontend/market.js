f8e0186cefad1a15dbccef3b52d8c551fd835c09
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-06-02 19:42:43 +0200

    Update market.js

--- frontend/market.js
+++ frontend/market.js
@@ -1,16 +1,19 @@
 const productsThumbnailDiv = document.getElementById('products-thumbnail-div')
 
 const fetchData =async()=>{
-    const fetched = await fetch(`${window.location.origin}/market`)
+    const fetched = await fetch(`${window.location.origin}/all-users`)
     const results = await fetched.json();
     console.log(results)
     results.forEach((el)=>{
         productsThumbnailDiv.innerHTML += `
         <div class="products-thumbnail">
-                <img class="search-thumbnail" src="istockphoto-1419246808-612x612.jpg"/>
+                <img class="search-thumbnail" src="pharmacy.png"/>
                 <div class="search-thumbnail-details">
-                    <p>${el.user_id.name}</p>
-                    <p>${el.tradeName}</p>
+                    <p>${el.name}
+                    <a 
+                    href='https://www.google.com/maps/search/?api=1&query=${el.location[0]},${el.location[1]}'
+                    ><img src="location.png"/></a></p>
+                    
                 </div>
             </div>
     `
