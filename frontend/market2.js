7ce8696a60b319e9c7b1eb336b74a2c1ce89ec50
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-06-12 15:53:17 +0200

    Update market2.js

--- frontend/market2.js
+++ frontend/market2.js
@@ -11,7 +11,7 @@ const fetchData =async()=>{
     productsThumbnailDivB.textContent = '';
     var resultsFiltered = results.filter((el)=>{
         
-        var city = el.city ? el.city : 'lilongwe'
+        var city = el.user_id.city ? el.user_id.city : 'lilongwe'
         return city.toLowerCase() == citySelected.value
     })
     if(citySelected.value == 'all'){
@@ -23,13 +23,24 @@ const fetchData =async()=>{
         <div class="products-thumbnail">
                 <img class="search-thumbnail" src="istockphoto-1419246808-612x612.jpg"/>
                 <div class="search-thumbnail-details">
-                    <p>${el.tradeName}${el.price}
-                    <a 
-                    href='https://www.google.com/maps/search/?api=1&query=${el.user_id.location[0]},${el.user_id.location[1]}'
-                    ><img src="location2.svg"/></a></p>
+                    <div class="product-details">
+                        <p class="text-center">${el.tradeName} ${el.drugStrength}</p>
+                    </div>
                     
+                    <div class="product-details">
+                        <p>MWK ${el.price?el.price:'N/A'}<img src="cart.svg" class="icon"/> </p>
+                    </div>
+                    
+                    <div class="product-details">
+                    <p>${el.user_id.name}<a 
+                    href='https://www.google.com/maps/search/?api=1&query=${el.user_id.location[0]},${el.user_id.location[1]}'
+                    ><img src="location2.svg" class="icon"/></a>
+                    </p>
+                    </div>
                 </div>
+                
             </div>
+            
     `
     })
     productsThumbnailDivB.appendChild(productsThumbnailDiv)
