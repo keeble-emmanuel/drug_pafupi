6b80797466a3cea7e2f222f3ad8c2cbde361b505
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-06-12 15:30:31 +0200

    Add files via upload

---
+++ frontend/market2.js
@@ -0,0 +1,43 @@
+const productsThumbnailDivB = document.getElementById('products-thumbnail-div-b');
+const citySelected = document.getElementById('cities');
+let resultsFiltered;
+
+const fetchData =async()=>{
+    const fetched = await fetch(`${window.location.origin}/all-products`)
+    const results = await fetched.json();
+    console.log(results);
+    const productsThumbnailDiv = document.createElement('div');
+    productsThumbnailDiv.id = "products-thumbnail-div";
+    productsThumbnailDivB.textContent = '';
+    var resultsFiltered = results.filter((el)=>{
+        
+        var city = el.city ? el.city : 'lilongwe'
+        return city.toLowerCase() == citySelected.value
+    })
+    if(citySelected.value == 'all'){
+        resultsFiltered =  results;
+    }
+    console.log(resultsFiltered)
+    resultsFiltered.forEach((el)=>{
+        productsThumbnailDiv.innerHTML += `
+        <div class="products-thumbnail">
+                <img class="search-thumbnail" src="istockphoto-1419246808-612x612.jpg"/>
+                <div class="search-thumbnail-details">
+                    <p>${el.tradeName}${el.price}
+                    <a 
+                    href='https://www.google.com/maps/search/?api=1&query=${el.user_id.location[0]},${el.user_id.location[1]}'
+                    ><img src="location2.svg"/></a></p>
+                    
+                </div>
+            </div>
+    `
+    })
+    productsThumbnailDivB.appendChild(productsThumbnailDiv)
+    
+}
+fetchData();
+
+citySelected.addEventListener('change', ()=>{
+    //alert('eee')
+    fetchData()
+})
