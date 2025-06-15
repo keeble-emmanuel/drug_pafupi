299d1570ae554ddf57f837adf0364c9f5c001fda
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-06-15 18:09:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,16 @@ app.get('/new_product', (req, res)=>{
 app.get('/keeble', (req, res)=>{
     res.sendFile(__dirname + "/frontend/master_account.html")
 })
+//404 page
+app.get('/not-found', (req, res)=>{
+    res.sendFile(__dirname + "/frontend/404page.html")
+})
 //delete product
 app.get('/deleteProduct/:productId', controller.deleteProduct)
+//promote product
+app.post('/promote-product', controller.promoteProduct)
+//depromote product
+app.post('/depromote-product', controller.depromoteProduct)
 //get all-users
 app.get('/all-users', controller.getAllUsers)
 //get specific user
@@ -29,7 +37,9 @@ app.get('/get_user/:user_id', controller.getUserDetails)
 app.post('/new-user', controller.creatNewUser)
 //update location
 app.post('/update-location', controller.updateLocation)
-//
+//change password
+app.post('/update-location', controller.updateLocation)
+//delete user
 app.get('/keeble/delete-user/:idtodelete', controller.deleteUser)
 //create product
 app.post('/new-product', controller.createNewDrug)
@@ -41,7 +51,10 @@ app.get('/searched-page/:genericName/:tradeName', controller.searchedPage)
 app.get('/all-products', controller.marketDisplay)
 //sign-in
 app.post('/sign-in', controller.signInfunx)
+//
 app.get('/getproducts/:user_id',  controller.getUserproducts )
+//change password
+app.post('/change-password', controller.changePassword)
 
 app.listen(PORT, ()=>{
     console.log('app listening')
