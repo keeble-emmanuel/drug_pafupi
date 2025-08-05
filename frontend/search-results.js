6cfb7ff88901a9e454831bf93d945e32cc61846a
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-08-05 02:59:45 +0000

    fixed

--- frontend/search-results.js
+++ frontend/search-results.js
@@ -7,7 +7,8 @@ const loadingScreen = document.getElementById('loading-screen')
 
 const searchFiters = JSON.parse(localStorage.getItem("searchfilter")) || [];
 nameDisplay.textContent  = searchFiters[0].genericName +' (' + searchFiters[0].tradeName + ')'
-
+var allData = []
+var filtered = []
 const fetchData1 =async()=>{
     const fetched = await fetch(`${window.location.origin}/searched-page`,{
         method: 'POST',
@@ -20,6 +21,7 @@ const fetchData1 =async()=>{
         })
     })
     const results = await fetched.json();
+    allData = results;
     console.log(results)
     //
     var drugstrengthArray = [];
@@ -66,129 +68,27 @@ const fetchData1 =async()=>{
         drugstrengthArray.unshift(el.drugStrength)
     })
     var drugstrengthArray2 = [... new Set(drugstrengthArray)];
-    console.log(drugstrengthArray, drugstrengthArray2)
-    
-    //filter by city
-    var resultsFiltered = results.filter((el)=>{
-        if(citySelected.value == 'all'){
-            return resultsFiltered =  results;
-        }else{
-            var city = !el.user_id.city ?'other': !citiesz.includes(el.user_id.city.toLowerCase().trim())? 'other':el.user_id.city;
-            return city.toLowerCase().trim() == citySelected.value
-        }
-        
-    })
-    console.log(resultsFiltered, '0', document.getElementById('strength-select').value)
-    //
-    var resultsFiltered1 = resultsFiltered.filter((el)=>{
-        if(document.getElementById('strength-select').value == 'all'){
-            console.log(resultsFiltered)
-            return resultsFiltered1 = resultsFiltered
-        }else{
-            return el.drugStrength == document.getElementById('strength-select').value
-        }
-        
-    })
-    console.log(resultsFiltered1)
-    //
-    var resultsFiltered2 = resultsFiltered1.filter((el)=>{
-         if(document.getElementById('formulation-select').value == 'all'){
-            return resultsFiltered2 = resultsFiltered1
-        }else{
-            return el.dosageForm == document.getElementById('formulation-select').value
-        }
-        
-    })
-    console.log(resultsFiltered2)
-    //
-    var priceArray = []
-    var avg_price = 0
-    resultsFiltered2.forEach((el)=>{
-        priceArray.unshift(el.price)
-        console.log(priceArray,'ooo')
-        
-    })
-    console.log(priceArray)
-    var total = priceArray.reduce((a, b)=>{
-            return parseFloat(a) + parseFloat(b)
-        }, 0)
-    console.log(total, "total", resultsFiltered2.length)
-    var divded =resultsFiltered2.length
-    avg_price = parseFloat(total) / parseFloat(divded)
-    console.log(avg_price, divded)
-    avg_price_display.textContent = 'AVG_Price: K'+ avg_price.toFixed(2)
-    //
-    
-    resultsFiltered2.forEach((el)=>{
-        var img = el.dosageForm == 'tablet' || el.dosageForm == 'capsules' ?'download.png':
-            el.dosageForm == 'solution' || el.dosageForm == 'powder-for-reconstitution'?'istockphoto-1304499871-612x612.jpg':
-            el.dosageForm == 'syrup'?'syrup.avif':
-            el.dosageForm =='ointment'? 'gettyimages-182665593-612x612.jpg':'eye.jpg'
-        productsThumbnailDiv.innerHTML += `
-        <div class="products-thumbnail">
-                <div id="product-thumbnail-name-pcy">
-                    <p >${el.user_id.name} pharmacy</p>
-                </div>
-                <div class="search-thumbnail-details">
-                    <div class="product-details bar">
-                        <p><b>exp:${el.expiryDate?el.expiryDate.substring(0, 7):'N/A'} </b></p>
-                    </div>
-                    <div class="product-details bar">
-                        <p>${el.drugStrength?el.drugStrength:'N/A'} </p>
-                    </div>
-                    <div class="location-icon bar">
-                        <a 
-                        href='https://www.google.com/maps/search/?api=1&query=${el.user_id.location[0]},${el.user_id.location[1]}'
-                        ><img src="location2.svg" class="icon"/>
-                        </a>
-                        
-                    </div>
-                </div>
-                
-            </div>
-            
-    `
-    })
-    productsThumbnailDivB.appendChild(productsThumbnailDiv)
-    loadingScreen.style.display = 'none';
-    //
-    
+    console.log(drugstrengthArray, drugstrengthArray2) 
     
 }
 fetchData1()
 
-const fetchData =async()=>{
-    loadingScreen.style.display = 'grid'
-     const fetched = await fetch(`${window.location.origin}/searched-page`,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            generic:searchFiters[0].genericName,
-            trade:searchFiters[0].tradeName
-        })
-    })
-    const results = await fetched.json();
-    console.log(results)
-
-    const productsThumbnailDiv = document.createElement('div');
-    productsThumbnailDiv.id = "products-thumbnail-div";
-    productsThumbnailDivB.textContent = '';
+const fetchData =async(params)=>{
+   
     var citiesz = ['zomba', 'blantyre', 'lilongwe', 'mzuzu']
     //
     var drugstrengthArray = [];
     
-    results.forEach((el)=>{
+    params.forEach((el)=>{
         drugstrengthArray.unshift(el.drugStrength)
     })
     var drugstrengthArray2 = [... new Set(drugstrengthArray)];
     console.log(drugstrengthArray, drugstrengthArray2)
     
     //filter by city
-    var resultsFiltered = results.filter((el)=>{
+    var resultsFiltered = params.filter((el)=>{
         if(citySelected.value == 'all'){
-            return resultsFiltered =  results;
+            return resultsFiltered =  params;
         }else{
             var city = !el.user_id.city ?'other': !citiesz.includes(el.user_id.city.toLowerCase().trim())? 'other':el.user_id.city;
             return city.toLowerCase().trim() == citySelected.value
@@ -234,9 +134,15 @@ const fetchData =async()=>{
     avg_price = isNaN(parseFloat(total) / parseFloat(divded)) ? 0.00 : parseFloat(total) / parseFloat(divded)
     console.log(avg_price, divded)
     avg_price_display.textContent = 'AVG_Price: K'+ avg_price.toFixed(2)
-    //
+    filtered = resultsFiltered2
     
-    resultsFiltered2.forEach((el)=>{
+}
+
+const displaynow=async(params)=>{
+    const productsThumbnailDiv = document.createElement('div');
+    productsThumbnailDiv.id = "products-thumbnail-div";
+    productsThumbnailDivB.textContent = '';
+    params.forEach((el)=>{
         
         productsThumbnailDiv.innerHTML += `
         <div class="products-thumbnail">
@@ -265,16 +171,25 @@ const fetchData =async()=>{
     })
     productsThumbnailDivB.appendChild(productsThumbnailDiv)
     loadingScreen.style.display = 'none';
-    
 }
-fetchData()
 
-citySelected.addEventListener('change', ()=>{   
-    fetchData();
+const organise=async()=>{
+    await fetchData1();
+    await fetchData(allData)
+    displaynow(filtered)
+}
+
+organise()
+
+citySelected.addEventListener('change', async()=>{   
+    await fetchData(allData)
+    displaynow(filtered)
 })
-document.getElementById('strength-select').addEventListener('change', ()=>{
-    fetchData();
+document.getElementById('strength-select').addEventListener('change', async()=>{
+    await fetchData(allData)
+    displaynow(filtered)
 })
-document.getElementById('formulation-select').addEventListener('change', ()=>{
-    fetchData();
+document.getElementById('formulation-select').addEventListener('change', async()=>{
+    await fetchData(allData)
+    displaynow(filtered)
 })
